bb7d8ec1cef06f6bbbd5b7f2ca9a422b0b3149a3
Alexander Matveev <blackkorol@gmail.com>
2017-03-20 21:08:20 +0300

    done

--- app/js/index/security.js
+++ app/js/index/security.js
@@ -8,6 +8,10 @@ var Promise = require('bluebird')
 var Registry = require('winreg')
 const crypto = require('crypto')
 var config = require('./config/default')
+
+
+var unique = require('node-machine-id');
+var machineId = unique.machineId, machineIdSync = unique.machineIdSync;
 var host = config.App.hostname
 
 function checkLicense(cb) {
@@ -31,6 +35,14 @@ function checkLicense(cb) {
 
   } else if (process.platform == 'darwin') {
     cb('ok')
+    // var obj = {};
+    // obj['memUserDir'] = memUserDir(); 
+    // obj['DiskEnum'] = os.cpus()[0].model; // os.cpus()[0].speed
+    // obj['BIOSVersion'] = machineIdSync({original: true});
+    // obj['BaseBoardManufacturer'] = '-';  
+    // obj['BIOSVendor'] = os.hostname(); 
+    // obj['SystemManufacturer'] = os.platform();
+    
     // var sendData = memUserDir()+"|"+obj["BIOSVersion"]+"|"+obj["DiskEnum"]+
     //   "|"+obj["BIOSVendor"]+"|"+obj["SystemManufacturer"]+"|"+obj["BaseBoardManufacturer"];
     // var serialKey = obj['memUserDir']+"|"+obj["BIOSVersion"]+"|"+obj["DiskEnum"];
